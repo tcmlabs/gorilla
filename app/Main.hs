d0a542105a4912ea8c19e5ee2e30d4c3510d1ced
rcasetta <richard.casetta@gmail.com>
2018-09-14 17:54:34 +0200

    add turtle dependency and launch flake8

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Lib
+import           Lib
+import           Prelude                 hiding ( FilePath )
+import           Turtle
+
+parser :: Parser FilePath
+parser = optPath "flake8Path" 'f' "Flake8 path"
 
 main :: IO ()
-main = someFunc
+main = do
+  flake8Path <- options "Static code analyzer" parser
+  sh $ do
+    output <- inshell (format fp flake8Path <> " **/*.py") stdin
+    liftIO $ print output
